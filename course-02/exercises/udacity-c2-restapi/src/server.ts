9af8e803a7c36b88e0d31a385df941f3edde2954
getssh <gtamrat33@gmail.com>
2022-12-29 21:16:21 +0300

    handled get and patch endpoint and dployment to elasticbeanstalk

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
-import express from 'express';
+import express, {Request,Response,NextFunction} from 'express';
+// import express from 'express';
 import { sequelize } from './sequelize';
 
 import { IndexRouter } from './controllers/v0/index.router';
@@ -18,7 +19,7 @@ import { V0MODELS } from './controllers/v0/model.index';
   app.use(bodyParser.json());
 
   //CORS Should be restricted
-  app.use(function(req, res, next) {
+  app.use(function(req: Request, res: Response, next:NextFunction) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8100");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
@@ -27,7 +28,7 @@ import { V0MODELS } from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter)
 
   // Root URI call
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send( "/api/v0/" );
   } );
   
